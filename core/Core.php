2b2b3f8232f95b1bdd68764ef1fb9c0a26722720
Sp4ceB1rd <sp4ceb1rd@gmail.com>
2013-12-02 18:27:43 +0100

    Add core

---
+++ core/Core.php
@@ -0,0 +1,23 @@
+<?php
+
+Class Core {
+
+	function __construct()
+	{
+
+	}
+
+	/*
+	*Loading all needed objects
+	 */
+	function start()
+	{
+		
+	}
+
+	function end()
+	{
+
+	}
+
+}
--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
 
-<pre>
-	<?php
-		echo dirname(dirname(__FILE__)).PHP_EOL;
-		echo basename(__DIR__).PHP_EOL;
-		substr(string, start)
-	?>
-</pre>
+<?php
+
+
+require_once __DIR__.'/../core/Core.php';
+
+$core = new Core();
+$core->start();
+
+
+$core->end();
+
